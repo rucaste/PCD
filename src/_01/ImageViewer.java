01a710494be00f49099a23b8741e4424187f1007
gfa <rui.m.castelhano@gmail.com>
2018-09-23 23:27:55 +0100

    e

--- src/_01/ImageViewer.java
+++ src/_01/ImageViewer.java
@@ -2,58 +2,114 @@ package _01;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ImageViewer {
 
-        private JFrame frame;
-        private ImageIcon icon;
-        private JLabel jlabeç;
-        private int i;
+    private JFrame frame;
+    private JButton rightButtom = new JButton("Next");
+    private JButton downButton = new JButton("Refresh");
+    private JButton leftButton = new JButton("Prev");
+
+    private JLabel image = new JLabel();
+
+    private ImageIcon icon;
+    private JLabel jlabel;
+
+    private int i;
+    private int size;
+
+    private ArrayList<File> files;
+    private ArrayList<ImageIcon> imageIcons = new ArrayList<>();
+
+    private ListenerForPrevious listenerForPrevious = new ListenerForPrevious();
+    private ListenerForNext listenerForNext = new ListenerForNext();
+    private ListenerForDown listenerForDown = new ListenerForDown();
 
 
-        public ImageViewer(String title, File[] files) {
-            i=0;
-            frame = new JFrame(title);
+    public ImageViewer(String title) {
 
-            frame.setLayout(new BorderLayout());
+        refresh();
 
-            frame.setSize(600,400);
-            frame.setLocation(300, 200);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        i=0;
+        frame = new JFrame(title);
 
-            frame.add(new JButton("Prev"), BorderLayout.WEST);
-            frame.add(new JButton("Nex"), BorderLayout.EAST);
-            frame.add(new JButton("Refresh"), BorderLayout.SOUTH);
+        this.files = files;
 
-            JLabel image = new JLabel();
+        frame.setLayout(new BorderLayout());
 
-            frame.add(image);
+        frame.setSize(600,400);
+        frame.setLocation(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            ImageIcon imageIcon = new ImageIcon(String.valueOf(files[i]));
+        frame.add(leftButton, BorderLayout.WEST);
+        frame.add(rightButtom, BorderLayout.EAST);
+        frame.add(downButton, BorderLayout.SOUTH);
 
-            imageIcon = getScaledImage(imageIcon, 500, 350);
+        frame.add(image);
 
-            image.setIcon(imageIcon);
+        image.setIcon(imageIcons.get(i));
 
-            frame.setVisible(true);
+        frame.setVisible(true);
+
+        leftButton.addActionListener(listenerForPrevious);
+        rightButtom.addActionListener(listenerForNext);
+        downButton.addActionListener(listenerForDown);
+    }
+
+    private void setImage(int i){
+        image.setIcon(imageIcons.get(i));
+    }
+
+    public void nextImage(){
+        if(i < size - 1) {
+            i++;
+            setImage(i);
         }
+    }
 
-        private Image getScaledImage(Image srcImg, int w, int h){
-            BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-            Graphics2D g2 = resizedImg.createGraphics();
+    public void previousImage(){
+        if(i > 0) {
+            i--;
+            setImage(i);
+        }
+    }
 
-            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-            g2.drawImage(srcImg, 0, 0, w, h, null);
-            g2.dispose();
+    public void refresh(){
+        File f = new File("/home/rui/IdeaProjects/PCD/src/_01/pictures");
+        files = new ArrayList<File>(Arrays.asList(f.listFiles()));
+        for(File file:files){
+            imageIcons.add(new ImageIcon(String.valueOf(file)));
+        }
+        size = files.size();
+    }
 
-            return resizedImg;
+    private class ListenerForPrevious implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            previousImage();
+        }
+    }
+
+    private class ListenerForNext implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            nextImage();
+        }
+    }
+
+    private class ListenerForDown implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            refresh();
+            i=0;
+        }
     }
 
 
 
 
-
-
 }
+
--- src/_01/Main.java
+++ src/_01/Main.java
@@ -5,21 +5,15 @@ import java.io.FileFilter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        File file = new File("/home/rui/IdeaProjects/PCD/src/_01/pictures");
-        File[] files = file.listFiles();
-        for(File f: files){
-            System.out.println(f.getName());
-        }
-
-        ImageViewer imageViewer = new ImageViewer("Gallery", files);
-
-        //ImageViewer imageViewer = new ImageViewer("Gallery");
+        ImageViewer imageViewer = new ImageViewer("Gallery");
 
     }
 }
